3e7c60d877b5d701c2f73fb28af554a4db6ef19d
Yixian Du <duyixian1234@qq.com>
2022-07-01 05:41:20 +0000

    feat(app): 修改评论区域
    1. 移除最终评价组件
    2. 支持快捷键提交内容

--- src/whiteBoard.js
+++ src/whiteBoard.js
@@ -4,7 +4,6 @@ import {
   Button,
   Col,
   Row,
-  Select,
   Layout,
   Space,
   Card,
@@ -18,44 +17,6 @@ import { nanoid } from "nanoid";
 import { getContent, init, setContent } from "./storage";
 
 const { Content } = Layout;
-
-function Conclusion() {
-  return (
-    <>
-      <Col span={6}>
-        <Space size="middle">
-          <span>结论：</span>
-          <Select defaultValue="不通过">
-            <Select.Option value="不通过">不通过</Select.Option>
-            <Select.Option value="通过">通过</Select.Option>
-          </Select>
-        </Space>
-      </Col>
-    </>
-  );
-}
-
-function Grade({ type }) {
-  return (
-    <>
-      <Col span={6}>
-        <Space size="middle">
-          <span>{type + "："}</span>
-          <Select defaultValue="A">
-            <Select.Option value="A">A</Select.Option>
-            <Select.Option value="A-">A-</Select.Option>
-            <Select.Option value="B">B</Select.Option>
-            <Select.Option value="B-">B-</Select.Option>
-            <Select.Option value="C">C</Select.Option>
-            <Select.Option value="C-">C-</Select.Option>
-            <Select.Option value="D">D</Select.Option>
-            <Select.Option value="D-">D-</Select.Option>
-          </Select>
-        </Space>
-      </Col>
-    </>
-  );
-}
 
 function Comments() {
   const [loading, setLoading] = React.useState(true);
@@ -74,6 +35,7 @@ function Comments() {
       }) || []
     );
     setLoading(false);
+    // eslint-disable-next-line
   }, []);
   React.useEffect(() => {
     if (loading) return;
@@ -104,64 +66,65 @@ function Comments() {
     document.body.removeChild(element);
   }
 
+  function handleKeyUp(event) {
+    if (event.keyCode === 13 && event.ctrlKey) {
+      changeComments();
+    }
+  }
+
   return (
     <>
-      <Row>
-        <Col span={24}>
-          <Row>
-            <Input.TextArea
-              placeholder="请输入评论或记录面试流程"
-              onChange={changeComment}
-              value={comment}
-            ></Input.TextArea>
-          </Row>
-          <Row style={{ paddingTop: "20px", paddingBottom: "20px" }}>
-            <Space size="middle">
-              <Button onClick={changeComments} type="default">
-                记录
-              </Button>
-              <Button onClick={removeLastComment}>删除上一条</Button>
-            </Space>
-          </Row>
-        </Col>
+      <Col span={8}>
+      <Row
+      style={{paddingTop:"20px"}}
+      >
+      <Card title="实时记录" size="default" bordered={true} style={{width:"400px"}}>
+        <Row>
+          <Col span={24}>
+            <Row>
+              <Input.TextArea
+                placeholder="请输入评论或记录面试流程。Ctrl + Enter 提交内容"
+                onChange={changeComment}
+                onKeyUp={handleKeyUp}
+                value={comment}
+              ></Input.TextArea>
+            </Row>
+            <Row style={{ paddingTop: "20px", paddingBottom: "20px" }}>
+              <Space size="middle">
+                <Button onClick={changeComments} type="default">
+                  记录
+                </Button>
+                <Button onClick={removeLastComment}>删除上一条</Button>
+              </Space>
+            </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={24}>
+            <Card
+              title="面试记录与评价"
+              size="default"
+              bordered={true}
+              extra={
+                <Button onClick={copy} type="primary">
+                  点击复制
+                </Button>
+              }
+            >
+              <Timeline>
+                {comments.map(({ ts, comment }) => (
+                  <Timeline.Item key={ts}>{`${ts.format(
+                    "HH:mm:ss"
+                  )} ${comment}`}</Timeline.Item>
+                ))}
+              </Timeline>
+            </Card>
+          </Col>
+        </Row>
+      </Card>
       </Row>
-      <Row>
-        <Col span={24}>
-          <Card
-            title="面试记录与评价"
-            size="default"
-            bordered={true}
-            extra={
-              <Button onClick={copy} type="primary">
-                点击复制
-              </Button>
-            }
-          >
-            <Timeline>
-              {comments.map(({ ts, comment }) => (
-                <Timeline.Item key={ts}>{`${ts.format(
-                  "HH:mm:ss"
-                )} ${comment}`}</Timeline.Item>
-              ))}
-            </Timeline>
-          </Card>
-        </Col>
-      </Row>
+      </Col>
     </>
-  );
-}
-
-function ConclusionBox() {
-  return (
-    <Col span={8}>
-      <Row style={{ padding: "20px", paddingTop: "10px" }}>
-        <Conclusion />
-        <Grade type="思路" />
-        <Grade type="代码实现" />
-        <Grade type="沟通" />
-      </Row>
-      <Comments />
-    </Col>
   );
 }
 
@@ -228,8 +191,7 @@ export function WhiteBoard() {
         <Content>
           <Row>
             <Room />
-            <Col span={2}></Col>
-            <ConclusionBox />
+            <Comments />
           </Row>
           <BackTop />
         </Content>
