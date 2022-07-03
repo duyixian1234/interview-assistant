edb3441c7b2cbc1b66e31b5d19bf403a95c4ce3f
Yixian Du <duyixian1234@qq.com>
2022-07-03 12:06:21 +0800

    feat(app): 接入腾讯云监控

--- src/index.js
+++ src/index.js
@@ -6,6 +6,15 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { sendToVercelAnalytics } from "./vitals";
 
+import Aegis from "aegis-web-sdk";
+
+new Aegis({
+  id: "bR4EqI6RxQqJabr9y4",
+  reportApiSpeed: true,
+  reportAssetSpeed: true,
+  spa: true,
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
